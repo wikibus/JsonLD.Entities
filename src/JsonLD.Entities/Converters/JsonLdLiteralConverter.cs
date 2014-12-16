f6e69e6ae2fdcf5c067b265605acffd05c902de2
tpluscode <tpluscode@users.noreply.github.com>
2014-12-16 13:31:35 +0100

    less comment

--- src/JsonLD.Entities/Converters/JsonLdLiteralConverter.cs
+++ src/JsonLD.Entities/Converters/JsonLdLiteralConverter.cs
@@ -12,9 +12,6 @@ namespace JsonLD.Entities.Converters
         /// <summary>
         /// Writes the JSON representation of the object.
         /// </summary>
-        /// <param name="writer">The <see cref="T:Newtonsoft.Json.JsonWriter" /> to write to.</param>
-        /// <param name="value">The value.</param>
-        /// <param name="serializer">The calling serializer.</param>
         public override void WriteJson(JsonWriter writer, [AllowNull] object value, JsonSerializer serializer)
         {
             writer.WriteValue(value);
@@ -23,13 +20,6 @@ namespace JsonLD.Entities.Converters
         /// <summary>
         /// Reads the JSON representation of the object.
         /// </summary>
-        /// <param name="reader">The <see cref="T:Newtonsoft.Json.JsonReader" /> to read from.</param>
-        /// <param name="objectType">Type of the object.</param>
-        /// <param name="existingValue">The existing value of object being read.</param>
-        /// <param name="serializer">The calling serializer.</param>
-        /// <returns>
-        /// The object value.
-        /// </returns>
         public override object ReadJson(JsonReader reader, Type objectType, [AllowNull] object existingValue, JsonSerializer serializer)
         {
             if (reader.TokenType != JsonToken.StartObject)
@@ -59,10 +49,6 @@ namespace JsonLD.Entities.Converters
         /// <summary>
         /// Determines whether this instance can convert the specified object type.
         /// </summary>
-        /// <param name="objectType">Type of the object.</param>
-        /// <returns>
-        ///   <c>true</c> if this instance can convert the specified object type; otherwise, <c>false</c>.
-        /// </returns>
         public override bool CanConvert(Type objectType)
         {
             return true;
