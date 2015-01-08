6d8f02fc111061f2e84333eb581d672a1632b8ca
tpluscode <tpluscode@users.noreply.github.com>
2015-01-08 12:48:10 +0100

    comment out surrogate pairs until eto.parse is updated

--- src/JsonLD.Entities/NQuadsParser.grammar.cs
+++ src/JsonLD.Entities/NQuadsParser.grammar.cs
@@ -18,8 +18,8 @@ namespace JsonLD.Entities
                                               | new CharRangeTerminal('\x2C00', '\x2FEF')
                                               | new CharRangeTerminal('\x3001', '\xD7FF')
                                               | new CharRangeTerminal('\xF900', '\xFDCF')
-                                              | new CharRangeTerminal('\xFDF0', '\xFFFD')
-                                              | new SurrogatePairRangeTerminal(0x10000, 0xEFFFF)).Named("PN_CHARS_BASE");
+                                              | new CharRangeTerminal('\xFDF0', '\xFFFD');
+                                              ////| new SurrogatePairRangeTerminal(0x10000, 0xEFFFF)).Named("PN_CHARS_BASE");
         private static readonly Parser PN_CHARS_U = (PN_CHARS_BASE | '_' | ':').Named("PN_CHARS_U");
         private static readonly Parser ECHAR = ('\\' & new CharSetTerminal('t', 'b', 'n', 'r', 'f', '"', '\\')).Named("ECHAR");
         private static readonly Parser PN_CHARS = (PN_CHARS_U | '-' | Terminals.Digit | '\x00B7' | new CharRangeTerminal('\x0300', '\x036F') | new CharRangeTerminal('\x203F', '\x2040')).Named("PN_CHARS");
