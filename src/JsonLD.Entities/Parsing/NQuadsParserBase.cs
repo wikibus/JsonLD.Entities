9a91383a19995c31415dd06020b72ecf124c154b
tpluscode <tpluscode@users.noreply.github.com>
2015-01-09 19:16:20 +0100

    fix incorrectly matching space in iri

--- src/JsonLD.Entities/Parsing/NQuadsParserBase.cs
+++ src/JsonLD.Entities/Parsing/NQuadsParserBase.cs
@@ -31,13 +31,13 @@ namespace JsonLD.Entities.Parsing
         private static readonly Parser ECHAR = ('\\' & new CharSetTerminal('t', 'b', 'n', 'r', 'f', '"', '\\')).Named("ECHAR");
         private static readonly Parser PN_CHARS = (PN_CHARS_U | '-' | Terminals.Digit | '\x00B7' | new CharRangeTerminal('\x0300', '\x036F') | new CharRangeTerminal('\x203F', '\x2040')).Named("PN_CHARS");
         private static readonly Parser UCHAR = (("\\u" & HEX & HEX & HEX & HEX) | ("\\U" & HEX & HEX & HEX & HEX & HEX & HEX & HEX & HEX)).Named("UCHAR");
-        private static readonly Parser IRIREF_UNRESERVED = Terminals.AnyChar.Except(new CharSetTerminal('<', '>', '"', '{', '}', '|', '^', '`', '\\') | new CharRangeTerminal('\0', '\x20').Named("test"));
+        private static readonly Parser IRIREF_UNRESERVED = Terminals.AnyChar.Except(new CharSetTerminal('<', '>', '"', '{', '}', '|', '^', '`', '\\') | new CharRangeTerminal((char)0, (char)0x20));
         private static readonly Parser IRIREF = ('<' & -(IRIREF_UNRESERVED | UCHAR) & '>').Named("IRIREF");
         private static readonly Parser STRING_LITERAL_QUOTE = ('"' & -(!new CharSetTerminal('\x22', '\x5C', '\xA', '\xD') | ECHAR | UCHAR) & '"').Named("STRING_LITERAL_QUOTE");
         private static readonly Parser BLANK_NODE_LABEL = ("_:" & -(PN_CHARS_U | Terminals.Digit) & ~(-(PN_CHARS | '.') & PN_CHARS)).Named("BLANK_NODE_LABEL");
         private static readonly Parser LANGTAG = ('@' & +Terminals.Letter & -('-' & +Terminals.Letter)).Named("LANGTAG");
         private static readonly Parser Ws = -Terminals.SingleLineWhiteSpace;
-        private static readonly Parser literal = (STRING_LITERAL_QUOTE & ~(("^^" & IRIREF) | LANGTAG)).SeparateChildrenBy(Ws).Named("literal");
+        private static readonly Parser literal = (STRING_LITERAL_QUOTE & ~(("^^" & IRIREF) | LANGTAG)).SeparatedBy(Ws).Named("literal");
         private static readonly Parser comment = ~(Ws & '#' & -Terminals.AnyChar.Except(Terminals.Eol)).SeparatedBy(Ws);
         private static readonly Parser eol = (comment & Terminals.Eol) | -Terminals.Eol;
         // ReSharper restore InconsistentNaming
