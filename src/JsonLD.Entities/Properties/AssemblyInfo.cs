04a331196578c52c750b0179114bc62c9ea07ef7
Tomasz Pluskiewicz <tpluskiewicz@pgs-soft.com>
2016-03-04 10:42:59 +0100

    no more nuspec :sparkles: [skip ci]

--- src/JsonLD.Entities/Properties/AssemblyInfo.cs
+++ src/JsonLD.Entities/Properties/AssemblyInfo.cs
@@ -4,7 +4,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("JsonLD.Entities")]
 [assembly: AssemblyDescription("Simple POCO object seriallization for JSON-LD")]
 [assembly: AssemblyProduct("JsonLD.Entities")]
-[assembly: AssemblyCopyright("Copyright ©  2014 Tomasz Pluskiewicz")]
+[assembly: AssemblyCopyright("Copyright © 2014-2016 Tomasz Pluskiewicz")]
+[assembly: AssemblyCompany("Tomasz Pluskiewicz")]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ee2b4e4a-32c1-4636-9dc0-09fa135eed99")]
