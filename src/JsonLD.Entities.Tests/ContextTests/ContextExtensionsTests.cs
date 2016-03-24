c98935719ab2167a64e3508ff53ab690091f0adf
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2016-03-24 10:23:04 +0100

    utility to merge contexts so that there is only single array

---
+++ src/JsonLD.Entities.Tests/ContextTests/ContextExtensionsTests.cs
@@ -0,0 +1,128 @@
+﻿using JsonLD.Entities.Context;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+
+namespace JsonLD.Entities.Tests.ContextTests
+{
+    [TestFixture]
+    public class ContextExtensionsTests
+    {
+        [Test]
+        public void Should_combine_multiple_objects_into_an_array()
+        {
+            // given
+            var context1 = JObject.Parse("{ 'xsd': 'http://xsd.ns' }");
+            var context2 = JObject.Parse("{ 'foaf': 'http://foaf.ns' }");
+            var expected = JToken.Parse("[ { 'xsd': 'http://xsd.ns' }, { 'foaf': 'http://foaf.ns' } ]");
+
+            // when
+            var merged = context1.MergeWith(context2);
+
+            // then
+            Assert.That(JToken.DeepEquals(merged, expected));
+        }
+
+        [Test]
+        public void Should_return_original_if_no_additional_contexts_passed()
+        {
+            // given
+            var context = JObject.Parse("{ 'xsd': 'http://xsd.ns' }");
+
+            // when
+            var merged = context.MergeWith();
+
+            // then
+            Assert.That(merged, Is.SameAs(context));
+        }
+
+        [Test]
+        public void Should_return_original_if_nulls_are_passed()
+        {
+            // given
+            var context = JObject.Parse("{ 'xsd': 'http://xsd.ns' }");
+
+            // when
+            var merged = context.MergeWith(null, null);
+
+            // then
+            Assert.That(merged, Is.SameAs(context));
+        }
+
+        [Test]
+        public void Should_flatten_additonal_array_contexts()
+        {
+            // given
+            var context1 = JObject.Parse("{ 'xsd': 'http://xsd.ns' }");
+            var context2 = JToken.Parse("[ { 'sch': 'http://schema.org' }, { 'foaf': 'http://foaf.ns' } ]");
+            var expected = JToken.Parse("[ { 'xsd': 'http://xsd.ns' }, { 'sch': 'http://schema.org' }, { 'foaf': 'http://foaf.ns' } ]");
+
+            // when
+            var merged = context1.MergeWith(context2);
+
+            // then
+            Assert.That(JToken.DeepEquals(merged, expected));
+        }
+
+        [Test]
+        public void Should_append_to_original_if_it_is_array()
+        {
+            // given
+            var context1 = JToken.Parse("[ { 'sch': 'http://schema.org' }, { 'foaf': 'http://foaf.ns' } ]");
+            var context2 = JObject.Parse("{ 'xsd': 'http://xsd.ns' }");
+            var expected = JToken.Parse("[ { 'sch': 'http://schema.org' }, { 'foaf': 'http://foaf.ns' }, { 'xsd': 'http://xsd.ns' } ]");
+
+            // when
+            var merged = context1.MergeWith(context2);
+
+            // then
+            Assert.That(JToken.DeepEquals(merged, expected));
+        }
+
+        [Test]
+        public void Should_merge_multiple_arrays()
+        {
+            // given
+            var context1 = JToken.Parse("[ { 'sch': 'http://schema.org' }, { 'foaf': 'http://foaf.ns' } ]");
+            var context2 = JToken.Parse("[ { 'xsd': 'http://xsd.ns' }, { 'bibo': 'http://bibo.ns' } ]");
+            var context3 = JToken.Parse("[ { 'dc': 'http://dc.terms' }, { 'owl': 'http://owl.ns' } ]");
+            var expected = JToken.Parse(@"
+[ 
+    { 'sch': 'http://schema.org' },
+    { 'foaf': 'http://foaf.ns' },
+    { 'xsd': 'http://xsd.ns' },
+    { 'bibo': 'http://bibo.ns' },
+    { 'dc': 'http://dc.terms' },
+    { 'owl': 'http://owl.ns' }
+]");
+
+            // when
+            var merged = context1.MergeWith(context2, context3);
+
+            // then
+            Assert.That(JToken.DeepEquals(merged, expected));
+        }
+
+        [Test]
+        public void Should_merge_external_contexts()
+        {
+            // given
+            var context1 = JToken.Parse("[ { 'sch': 'http://schema.org' }, { 'foaf': 'http://foaf.ns' } ]");
+            var context2 = JToken.Parse("'http://external.ctx'");
+            var context3 = JToken.Parse("[ { 'dc': 'http://dc.terms' }, { 'owl': 'http://owl.ns' } ]");
+            var expected = JToken.Parse(@"
+[ 
+    { 'sch': 'http://schema.org' },
+    { 'foaf': 'http://foaf.ns' },
+    'http://external.ctx',
+    { 'dc': 'http://dc.terms' },
+    { 'owl': 'http://owl.ns' }
+]");
+
+            // when
+            var merged = context1.MergeWith(context2, context3);
+
+            // then
+            Assert.That(JToken.DeepEquals(merged, expected));
+        }
+    }
+}
---
+++ src/JsonLD.Entities/Context/ContextExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace JsonLD.Entities.Context
+{
+    /// <summary>
+    /// Extension to work with JSON-LD @contexts
+    /// </summary>
+    public static class ContextExtensions
+    {
+        /// <summary>
+        /// Merges the multiple @contexts by combining them within a single array.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="otherContexts">The other contexts.</param>
+        /// <returns>
+        /// original <paramref name="context"/> or a flattened <see cref="JArray"/>
+        /// </returns>
+        public static JToken MergeWith(this JToken context, params JToken[] otherContexts)
+        {
+            otherContexts = otherContexts.Where(ctx => ctx != null).ToArray();
+
+            if (otherContexts.Length == 0)
+            {
+                return context;
+            }
+
+            JToken mergedContext = context is JArray ? context : new JArray(context);
+
+            foreach (var token in SplitTokens(otherContexts))
+            {
+                ((JArray)mergedContext).Add(token);
+            }
+
+            return mergedContext;
+        }
+
+        private static IEnumerable<JToken> SplitTokens(IEnumerable<JToken> otherContexts)
+        {
+            foreach (var context in otherContexts)
+            {
+                if (context is JArray)
+                {
+                    foreach (var inner in SplitTokens(context))
+                    {
+                        yield return inner;
+                    }
+                }
+                else
+                {
+                    yield return context;
+                }
+            }
+        }
+    }
+}
